53399dd40d0e56008965ab6c6b3f1dcdfb4ffde8
Vincent Michler <60316541+vincm1@users.noreply.github.com>
2026-06-12 19:17:53 +0200

    hero section and navar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import vue from '@astrojs/vue';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import vue from "@astrojs/vue";
+import tailwindcss from "@tailwindcss/vite";
+import { storyblok } from "@storyblok/astro";
+import { loadEnv } from "vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    vue(),
-  ],
+  integrations: [vue()],
   vite: {
     plugins: [tailwindcss()],
   },
